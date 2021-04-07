787c7d6f9db91781e58d3f5029ef6365fdb25184
Naga Chaitanya Konada <knc@outlook.in>
2021-04-07 22:43:51 +0530

    Initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,121 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: "Naga Chaitanya Konada",
+  tagline: "Lets learn something new today",
+  url: "https://thebestdeveloper.me",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.png",
+  organizationName: "ChaituKNag", // Usually your GitHub org/user name.
+  projectName: "thebestdeveloper.me", // Usually your repo name.
+  themeConfig: {
+    sidebarCollapsible: true,
+    hideableSidebar: true,
+    colorMode: {
+      defaultMode: "dark",
+      disableSwitch: false,
+      respectPrefersColorScheme: true
+    },
+    navbar: {
+      title: "Naga Chaitanya Konada",
+      hideOnScroll: false,
+      style: "primary",
+      logo: {
+        alt: "This is the site of KNC",
+        src: "img/favicon.png"
+      },
+      items: [
+        // {
+        //   to: "docs/",
+        //   activeBasePath: "articles",
+        //   label: "Articles",
+        //   position: "left"
+        // },
+        { to: "blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/ChaituKNag",
+          label: "GitHub",
+          position: "right"
+        },
+        {
+          href: "https://thebestdeveloper.me",
+          label: "TBD.me",
+          position: "right"
+        }
+      ]
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Style Guide",
+              to: "docs/"
+            },
+            {
+              label: "Second Doc",
+              to: "docs/doc2/"
+            }
+          ]
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus"
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus"
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus"
+            }
+          ]
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "blog"
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus"
+            }
+          ]
+        }
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Naga Chaitanya Konada`
+    }
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        // docs: {
+        //   sidebarPath: require.resolve("./sidebars.js"),
+        //   // Please change this to your repo.
+        //   editUrl: "https://github.com/facebook/docusaurus/edit/master/website/"
+        // },
+        docs: false,
+        blog: {
+          showReadingTime: true,
+          blogSidebarCount: 0,
+          // editUrl:
+          //   "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+          postsPerPage: 5
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      }
+    ]
+  ]
+};
