ce4d1c76704b5b2f53ef83354c6dd4844819c1fe
Naga Chaitanya Konada <knc.dev@outlook.com>
2021-04-27 18:15:33 +0530

    refactor: feature section

---
+++ src/components/Feature.js
@@ -0,0 +1,18 @@
+import React from "react";
+import clsx from "clsx";
+import styles from "../pages/styles.module.css";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+export default function Feature({ imageUrl, title, description }) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx("col col--6 text--center", styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
